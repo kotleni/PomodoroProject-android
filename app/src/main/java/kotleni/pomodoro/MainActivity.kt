e43e70704181905ac53f6289e557aa327173b050
kotleni <yavarenikya@gmail.com>
2023-01-15 00:26:40 +0200

    Implemented basic fragments logic

--- app/src/main/java/kotleni/pomodoro/MainActivity.kt
+++ app/src/main/java/kotleni/pomodoro/MainActivity.kt
@@ -2,10 +2,35 @@ package kotleni.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotleni.pomodoro.databinding.ActivityMainBinding
+import kotleni.pomodoro.fragments.SettingsFragment
+import kotleni.pomodoro.fragments.TasksFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            replaceFragment(when(it.itemId) {
+                R.id.tasks -> TasksFragment()
+                R.id.settings -> SettingsFragment()
+                else -> throw Exception("Unknown itemId")
+            })
+            return@setOnItemSelectedListener true
+        }
+
+        replaceFragment(TasksFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(androidx.appcompat.R.anim.abc_fade_in, androidx.appcompat.R.anim.abc_fade_out)
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/kotleni/pomodoro/fragments/SettingsFragment.kt
@@ -0,0 +1,22 @@
+package kotleni.pomodoro.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotleni.pomodoro.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+    private val binding: FragmentSettingsBinding by lazy { FragmentSettingsBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
