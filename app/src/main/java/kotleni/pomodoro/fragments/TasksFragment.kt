dff6c5828525971ba34340cad08eb6f9126f2e45
kotleni <yavarenikya@gmail.com>
2023-01-18 22:55:35 +0200

    Removed unused imports

--- app/src/main/java/kotleni/pomodoro/fragments/TasksFragment.kt
+++ app/src/main/java/kotleni/pomodoro/fragments/TasksFragment.kt
@@ -1,7 +1,5 @@
 package kotleni.pomodoro.fragments
 
-import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
